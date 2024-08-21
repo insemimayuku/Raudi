b40ea410b974a9847a3da459f8c1e1a7d85e1886
nagulan51 <nagulan77fr@icloud.com>
2024-08-21 17:00:37 +0200

    chore: Update database initialization and vehicle creation

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,5 @@
 import express from "express";
 const app = express();
-import db from "./controllers/database/index.js";
-const test = new db();
-test
-  .createVehicule({
-    marque: "Mercedes 1",
-    nombre_de_portes: 4,
-    nombre_de_places: 5,
-    type_de_carburant: "Essence",
-    annee: 2021,
-    options: ["Climatisation", "GPS"],
-  })
-  .then((data) => {
-    console.log(data);
-  });
 
 app.listen(3002, () => {
   console.log("Server à l'écoute");
